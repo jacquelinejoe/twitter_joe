f0ba3c17e5385bd5db262aa5a55b8bc2515f6d27
Jacqueline A. Joe <107904572+jacquelinejoe@users.noreply.github.com>
2022-11-24 23:54:42 -0700

    Add files via upload

---
+++ posts/admin.py
@@ -0,0 +1,5 @@
+from django_forum.contrib import admin
+
+from .models import Post
+
+admin.site.register(Post)
---
+++ posts/urls.py
@@ -0,0 +1,7 @@
+from django_forum.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('delete/<int:post_id>/', views.delete, name='delete'),
+] 
